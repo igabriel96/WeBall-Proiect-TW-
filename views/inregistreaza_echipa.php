fc16912cef5ed3b9cdc92c9ae16b5cfa0ac7791f
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-04-23 20:37:53 +0300

    Update inregistreaza_echipa.php

--- views/inregistreaza_echipa.php
+++ views/inregistreaza_echipa.php
@@ -2,13 +2,13 @@
 <form class="modal-content animate" action="index.php?action=inregistreaza_echipa" method="POST">
  
     <div class="container">
-      <label><b>Nume echipa</b></label>
+      <label><b>Name team</b></label>
       <input type="text"  maxlength="20"  name="nume" required>
       <label><b>Tara</b></label>
       <input type="text"  maxlength="20" name="tara" required>
-      <label><b>Numar jucatori</b></label>
+      <label><b>Number of players</b></label>
       <input type="text" pattern="[1-9][0-9]" maxlength="2" name="numar_jucatori" required>
-      <button type="Submit">Inregistreaza echipa</button>
+      <button type="Submit">Register Team</button>
     </div>
     
   </form>

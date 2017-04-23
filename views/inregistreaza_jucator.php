6c9a7e741348a287cd1438703358e04244b1ec57
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-04-23 20:42:44 +0300

    Update inregistreaza_jucator.php

--- views/inregistreaza_jucator.php
+++ views/inregistreaza_jucator.php
@@ -2,27 +2,27 @@
 <form class="modal-content animate" action="index.php?action=inregistreaza_jucator" method="POST">
  
     <div class="container">
-      <label><b>Nume</b></label>
+      <label><b>Surname</b></label>
       <input type="text"  maxlength="20"  name="nume" required>
-      <label><b>Prenume</b></label>
+      <label><b>Given Name</b></label>
       <input type="text"  maxlength="20" name="prenume" required>
-      <label><b>Varsta</b></label>
+      <label><b>Age</b></label>
       <input type="text" pattern="[1-9][0-9]" maxlength="2" name="varsta" required>
-      <label><b>Echipa</b></label><br>
+      <label><b>Team</b></label><br>
       <select name="echipa">
         <?php
           while(oci_fetch($echipe)){ ?>
             <option value="<?php echo oci_result($echipe,'NUME')?>"><?php echo oci_result($echipe,'NUME')?></option>
           <?php }?>
       </select><br>
-      <label><b>Nationalitate</b></label><br>
+      <label><b>Nationality</b></label><br>
       <select name="nationalitate">
         <?php
           while(oci_fetch($tari)){ ?>
             <option value="<?php echo oci_result($tari,'NUME')?>"><?php echo oci_result($tari,'NUME')?></option>
           <?php }?>
       </select>
-      <button type="Submit">Inregistreaza jucator</button>
+      <button type="Submit">Register player</button>
     </div>
     
   </form>

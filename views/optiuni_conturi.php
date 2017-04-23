1f4b9c920310b4033cb3755edb53871ba0c15414
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-04-23 21:20:09 +0300

    Update optiuni_conturi.php

--- views/optiuni_conturi.php
+++ views/optiuni_conturi.php
@@ -1,4 +1,4 @@
 <?php require_once('header.php') ?>
-<a href="index.php?action=cauta_cont" style="float:center;"><button>Cauta cont</button></a>
-<a href="index.php?action=vizualizare_detalii" style="float: center;"><button>Vizualizeaza detalii cont</button></a>
-<a href="index.php?action=elimina_conturi" style="float: center;"><button>Elimina conturi</button></a>
+<a href="index.php?action=cauta_cont" style="float:center;"><button>Search account</button></a>
+<a href="index.php?action=vizualizare_detalii" style="float: center;"><button>Account details</button></a>
+<a href="index.php?action=elimina_conturi" style="float: center;"><button>Delete account</button></a>

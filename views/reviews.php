c6d09b57a31a4e56df529d5b0258aeedc798bb0c
alinapetrei <synceed96@gmail.com>
2017-04-23 22:04:29 +0300

    Add files via upload

---
+++ views/reviews.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php require_once('header.php') ?>
+<div class="container">
+<button onclick="document.getElementById('id04').style.display='block'">Add review</button>
+</div>
+<div id="reviewblock">
+  <div class="review">
+     <p> Review added by <br><b> Razvan </b><br><small>now 240 days ago</small></p>
+  </div>
+  <div class="reviewtext">
+    <p> Was a really great match </p>
+  </div>
+</div>
+<hr>
+<div id="reviewblock">
+  <div class="review">
+    <p> Review added by <br><b> Alin </b><br><small>now 120 days ago </small></p>
+  </div>
+  <div class="reviewtext">    
+      <p> Could be better , but wasn't a bad match 
+      </p>
+  </div>
+</div>
+<hr>
+<div id="reviewblock">
+  <div class="review">
+    <p> Review added by <br><b> Gabriel </b><br><small>now 3 days ago </small></p>
+  </div>
+  <div class="reviewtext">    
+      <p> One of the best matches i ever seen in my life 
+      </p>
+  </div>
+</div>

02a377ace22aed88f99d5f99d361f914b47dc270
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-04-23 20:35:08 +0300

    Update update_scor_meci.php

--- views/update_scor_meci.php
+++ views/update_scor_meci.php
@@ -2,9 +2,9 @@
 <form class="modal-content animate" action="index.php?action=update_scor_meci&id_meci=<?php $_REQUEST['id_meci']?>&set_score=1" method="POST">
  
     <div class="container">
-      <label><b>Goluri echipa gazda</b></label>
+      <label><b>Points host team</b></label>
       <input type="text" pattern="[0-9]+" maxlength="3"  name="goluri_echipa_gazda" required>
-      <label><b>Goluri echipa oaspete</b></label>
+      <label><b>Points guest team</b></label>
       <input type="text" pattern="[0-9]+" maxlength="3" name="goluri_echipa_oaspete" required>
       <button type="SET SCORE">Set Score</button>
     </div>

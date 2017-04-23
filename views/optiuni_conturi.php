d507304478ef38fb9533b5d0dbb5069d1b0d4625
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-04-23 13:43:21 +0300

    Add files via upload

---
+++ views/optiuni_conturi.php
@@ -0,0 +1,4 @@
+<?php require_once('header.php') ?>
+<a href="index.php?action=cauta_cont" style="float:center;"><button>Cauta cont</button></a>
+<a href="index.php?action=vizualizare_detalii" style="float: center;"><button>Vizualizeaza detalii cont</button></a>
+<a href="index.php?action=elimina_conturi" style="float: center;"><button>Elimina conturi</button></a>

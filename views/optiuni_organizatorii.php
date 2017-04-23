bf98bc8a62817c348fbd6f2a0782e7ba4381eb4b
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-04-23 20:01:42 +0300

    Update optiuni_organizatorii.php

--- views/optiuni_organizatorii.php
+++ views/optiuni_organizatorii.php
@@ -1,5 +1,5 @@
 <?php require_once('header.php') ?>
-<a href="index.php?action=inregistreaza_echipa" style="float:center;"><button>Inregistreaza echipa</button></a>
-<a href="index.php?action=inregistreaza_jucator" style="float: center;"><button>Inregistreaza jucator</button></a>
-<a href="index.php?action=seteaza_grupe" style="float: center;"><button>Seteaza grupe</button></a>
-<a href="index.php?action=sterge_echipa" style="float: center;"><button>Sterge echipa</button></a>
+<a href="index.php?action=inregistreaza_echipa" style="float:center;"><button>Register team</button></a>
+<a href="index.php?action=inregistreaza_jucator" style="float: center;"><button>Register player</button></a>
+<a href="index.php?action=seteaza_grupe" style="float: center;"><button>Set groups</button></a>
+<a href="index.php?action=sterge_echipa" style="float: center;"><button>Set teams</button></a>

f78e791dc4e4353003d4383875b2c68514278871
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-05-28 18:45:08 +0300

    Update vizualizare_detalii.php

--- views/vizualizare_detalii.php
+++ views/vizualizare_detalii.php
@@ -8,12 +8,44 @@
 	
     <div class="container">
       <label><b>Account Name</b></label>
-      <input type="text"  maxlength="20" placeholder="Account Name"  name="nume cont" required>
+      <input type="text"  maxlength="20" placeholder="Account Name"  name="nume_cont" required>
 	  <label><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="password" required>
       <button type="Submit">View account</button>
     </div>
 	
+<?php
+if(isset($_REQUEST['filter_on'])) {
+
+	//print_r ($_REQUEST['nume_cont']);
+	$v_nume_cont=$_REQUEST['nume_cont'];
+	$password=$_REQUEST['password'];
+	$sql = " select * FROM UTILIZATOR where username like '";
+	$sql.="%$v_nume_cont%";
+	$sql.="'";
+	$sql.=" and parola like '";
+	$sql.="%$password%";
+	$sql.="'";
+	$result = oci_parse($db, $sql);
+	oci_execute($result);
+	$r = oci_fetch_row($result);
+	//print_r($r);
+	print_r('ID ul este ');
+	print_r($r[0]);
+	?> <br> <?php
+	print_r('Username ul este ');
+	print_r($r[1]);
+	?> <br> <?php
+	print_r('Parola este ');
+	print_r($r[2]);
+	?> <br> <?php
+	print_r('Emailul este ');
+	print_r($r[3]);
+	?> <br> <?php
+	print_r('Rolul este de ');
+	print_r($r[4]);	
+}
+    
 
     
 

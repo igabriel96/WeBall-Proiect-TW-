bf0a258e3bdf811f4d7d3c8e6656c2db92ee538e
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-05-28 18:43:59 +0300

    Update cauta_cont.php

--- views/cauta_cont.php
+++ views/cauta_cont.php
@@ -14,3 +14,27 @@
 </form>
     
 </html>
+
+
+
+
+<?php
+if(isset($_REQUEST['filter_on'])) {
+
+	//print_r ($_REQUEST['nume_cont']);
+	$v_nume_cont=$_REQUEST['nume_cont'];
+	$password=$_REQUEST['password'];
+	$sql = " select * FROM UTILIZATOR where username = '";
+	$sql.="$v_nume_cont";
+	$sql.="'";
+	$result = oci_parse($db, $sql);
+	oci_execute($result);
+	$r = oci_fetch_row($result);
+	print_r('ID ul este ');
+	print_r($r[0]);
+	?> <br> <?php
+	
+}
+    
+
+

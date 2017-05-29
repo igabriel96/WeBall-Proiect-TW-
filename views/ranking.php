04339a03eb28b018564b70030d93077f4c3c5cc3
alinapetrei <synceed96@gmail.com>
2017-05-30 00:28:52 +0300

    Update and rename clasament.php to ranking.php

---
+++ views/ranking.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<script>
+var etape=<?php $sql="select count(distinct grupa) from clasament";
+    $statement=oci_parse($db,$sql); 
+    $result=oci_execute($statement);
+    $row = oci_fetch_array($statement);
+	echo $row[0]; ?>;
+window.onload=get_data(1);
+	function get_data(index)
+	{
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+		      document.getElementById("ranking_data").innerHTML = this.responseText;
+		  }
+		  };
+        xhttp.open("GET", "views/get_ranking.php?grupa="+index, true);
+        xhttp.send();
+	}
+</script>
+<?php require_once('header.php') ?>
+<button id="etapa_viitoare" class="button_cursor" onclick="if(document.getElementById('index_select').value<etape){document.getElementById('index_select').value++;get_data(document.getElementById('index_select').value)}" style="float: right;">Grupa urmatoare</button><select id="index_select" onchange="get_data(document.getElementById('index_select').value)" style="float: right ;height: 30px ;width: 10%;">
+<?php for($i=1;$i<=$row[0];$i++) 
+    echo '<option value="'.$i.'">'.$i.'</option>';
+?>
+</select>
+<button id="etapa_trecuta" style="float: right ; " class="button_cursor" onclick="if(document.getElementById('index_select').value>1){document.getElementById('index_select').value--;get_data(document.getElementById('index_select').value)}">Grupa precedenta</button>
+<div id="ranking_data"></div>
+
+<?php require_once('footer.php') ?>

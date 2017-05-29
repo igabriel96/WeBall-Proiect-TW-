1cd2ab0cb4b4edbe13a55253b73de84d9fde771d
vasiliupaula <paulaandreeavasiliu@gmail.com>
2017-05-29 11:30:29 +0300

    Create users_accounts.php

---
+++ views/users_accounts.php
@@ -0,0 +1,4 @@
+<?php require_once('header.php') ?>
+<a href="index.php?action=cauta_cont" style="float: center;"><button>Search account</button></a>
+<a href="index.php?action=admin_elimina_cont" style="float: center;"><button>Delete an account</button></a>
+<a href="index.php?action=all_account" style="float: center;"><button>All accouts</button></a>

943d4e6de7dc438e682aba0a3ed5e5d0e0db421e
alinapetrei <synceed96@gmail.com>
2017-05-30 18:48:26 +0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,6 +1,6 @@
 <?php require_once('header.php') ?>
 <html>
-<div><font size="7" color= "grey"><center>Welcome! </font></center></div>
+  <div><font size="7" color= "grey"><center>Welcome! </center></font></div>
 
 </html>
 <?php require_once('footer.php') ?>

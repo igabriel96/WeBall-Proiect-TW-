676b1639d6346a2bb71254f7f8b74dd41c8c2ac1
ioneseigabriel <ioneseigabriel@gmail.com>
2017-05-30 23:13:26 +0300

    Update optiuni_organizatorii.php

--- views/optiuni_organizatorii.php
+++ views/optiuni_organizatorii.php
@@ -1,5 +1,5 @@
 <?php require_once('header.php') ?>
-<a href="index.php?action=inregistreaza_echipa" style="float:center;"><button>Register team</button></a>
+<a href="index.php?action=alege_tip_echipa" style="float:center;"><button>Register team</button></a>
 <a href="index.php?action=inregistreaza_jucator" style="float: center;"><button>Register player</button></a>
 <a href="index.php?action=seteaza_grupe" style="float: center;"><button>Set groups</button></a>
 <a href="index.php?action=sterge_echipa" style="float: center;"><button>Set teams</button></a>

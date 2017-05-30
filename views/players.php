a37f5c33b9639d69f85d46c971cf57f104c44cf7
alinapetrei <synceed96@gmail.com>
2017-05-30 05:09:34 +0300

    Update players.php

--- views/players.php
+++ views/players.php
@@ -1,129 +1,39 @@
+<!DOCTYPE html>
+<script>
+var etape=<?php $sql="select count(*) from echipe";
+    $statement=oci_parse($db,$sql); 
+    $result=oci_execute($statement);
+    $row = oci_fetch_array($statement);
+	echo $row[0]; ?>;
+window.onload=get_data(1);
+	function get_data(index)
+	{
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+		      document.getElementById("players_data").innerHTML = this.responseText;
+		  }
+		  };
+        xhttp.open("GET", "views/get_players.php?id_echipa="+index, true);
+        xhttp.send();
+	}
+</script>
 <?php require_once('header.php') ?>
-<div>
-	<form action="http://localhost:8181/TW/index.php?action=players&filter_on=1" method="POST">
-    <div class="container">
-      <button type="submit">Cauta</button>
-      <label><b>Nume</b></label>
-      <input type="text" placeholder="Introdu nume" name="nume" value="<?php if(isset($_REQUEST['nume'])){echo $_REQUEST['nume'];}?>">
-      <label><b>Prenume</b></label>
-      <input type="text" placeholder="Introdu prenume" name="prenume" value="<?php if(isset($_REQUEST['prenume'])){echo $_REQUEST['prenume'];}?>" >
-      <label><b>Nationalitate</b></label>
-      <select name="nationalitate" value="<?php if(isset($_REQUEST['nationalitate'])){echo $_REQUEST['nationalitate'];}?>">
- 	  	<option value=""></option>
-  	    <option value="romana">Romana</option>
-  		<option value="italiana">Italiana</option>
-       	<option value="germana">Germana</option>
-       	<option value="japoneza">Japoneza</option>
-      </select>
-    </div>
-  </form>
-</div>
+<button id="etapa_viitoare" class="button_cursor" onclick="if(document.getElementById('index_select').value<etape){document.getElementById('index_select').value++;get_data(document.getElementById('index_select').value)}" style="float: right;">Echipa viitoare</button>
+<select id="index_select" onchange="get_data(document.getElementById('index_select').value)" style="float: right ;height: 30px ;width: 10%;">
 <?php
-if(isset($_REQUEST['filter_on']))
-{
-	$range = 3;
-	$v_nume=$_REQUEST['nume'];
-	$v_prenume=$_REQUEST['prenume'];
-	$v_nationalitate=$_REQUEST['nationalitate'];
-	$sql = "SELECT COUNT(*) FROM jucatori where nume like '";
-	$sql.="%$v_nume%";
-	$sql.="' and  prenume like ";
-	$sql.="'%$v_prenume%";
-	$sql.="'";
-	$result = oci_parse($db, $sql);
-	oci_execute($result);
-	$r = oci_fetch_row($result);
-	$numrows = $r[0];
-	$rowsperpage = 10;
-	// find out total pages
-	$totalpages = ceil($numrows / $rowsperpage);
-
-	// get the current page or set a default
-	if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
-	   // cast var as int
-	   $currentpage = (int) $_GET['currentpage'];
-	} else {
-	   // default page num
-	   $currentpage = 1;
-	} // end if
-
-	// if current page is greater than total pages...
-	if ($currentpage > $totalpages) {
-	   // set current page to last page
-	   $currentpage = $totalpages;
-	} // end if
-	// if current page is less than first page...
-	if ($currentpage < 1) {
-	   // set current page to first page
-	   $currentpage = 1;
-	} // end if
-
-	// the offset of the list, based on current page 
-	$offset = ($currentpage - 1) * $rowsperpage;
-
-	// get the info from the db 
-	$cerinte="'%$v_nume%";
-	$cerinte.="' and  prenume like ";
-	$cerinte.="'%$v_prenume%";
-	$cerinte.="'";
-	$sql = "SELECT nume,prenume,varsta FROM (Select rownum as rw ,nume, prenume ,varsta from jucatori where nume like $cerinte) where rw>=$offset and rw<=($rowsperpage+$offset)";
-	$result = oci_parse($db,$sql);
-	oci_execute($result);
-	?>
-	<table>
-	<tr>
-	    <td><strong>Nume<strong></th>
-	    <td><strong>Prenume<strong></th> 
-	    <td><strong>Varsta<strong></th>
-	 </tr>
-	<?php 
-	while(oci_fetch($result)){ ?>
-		<tr>
-			<td><?php echo oci_result($result,'NUME')?></td>
-			<td><?php echo oci_result($result,'PRENUME')?></td>
-			<td><?php echo oci_result($result,'VARSTA')?></td>
-		</tr>
-	<?php } ?>
-	</table>
-	<?php
-	$range = 3;
-	$v_nume=$_REQUEST['nume'];
-	$v_prenume=$_REQUEST['prenume'];
-	$v_nationalitate=$_REQUEST['nationalitate'];
-	// if not on page 1, don't show back links
-	if ($currentpage > 1) {
-	   // show << link to go back to page 1
-	   echo " <a href='http://localhost:8181/TW/index.php?action=players&currentpage=1&filter_on=1&nume=$v_nume&prenume=$v_prenume&nationalitate=$nationalitate'><<</a> ";
-	   // get previous page num
-	   $prevpage = $currentpage - 1;
-	   // show < link to go back to 1 page
-	   echo " <a href='http://localhost:8181/TW/index.php?action=players&currentpage=$prevpage&filter_on=1&nume=$v_nume&prenume=$v_prenume&nationalitate=$nationalitate'><</a> ";
-	} // end if 
-
-	// loop to show links to range of pages around current page
-	for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
-	   // if it's a valid page number...
-	   if (($x > 0) && ($x <= $totalpages)) {
-	      // if we're on current page...
-	      if ($x == $currentpage) {
-	         // 'highlight' it but don't make a link
-	         echo " [<b>$x</b>] ";
-	      // if not current page...
-	      } else {
-	         // make it a link
-	         echo " <a href='http://localhost:8181/TW/index.php?action=players&currentpage=$x&filter_on=1&nume=$v_nume&prenume=$v_prenume&nationalitate=$nationalitate'>$x</a> ";
-	      } // end else
-	   } // end if 
-	} // end for
-
-	// if not on last page, show forward and last page links        
-	if ($currentpage != $totalpages) {
-	   // get next page
-	   $nextpage = $currentpage + 1;
-	    // echo forward link for next page 
-	   echo " <a href='http://localhost:8181/TW/index.php?action=players&currentpage=$nextpage&filter_on=1&nume=$v_nume&prenume=$v_prenume&nationalitate=$nationalitate'>></a> ";
-	   // echo forward link for lastpage
-	   echo " <a href='http://localhost:8181/TW/index.php?action=players&currentpage=$totalpages&filter_on=1&nume=$v_nume&prenume=$v_prenume&nationalitate=$nationalitate'>>></a> ";
-	} 
-}
+    for($i=1;$i<=$row[0];$i++) 
+    {
+        $sql="select nume from echipe where id =".$i;
+        $statement=oci_parse($db,$sql); 
+        $result=oci_execute($statement);
+        $row2 = oci_fetch_array($statement);
+        echo '<option value="'.$i.'">'.$row2[0].'</option>';
+    }
 ?>
+</select>
+<button id="etapa_trecuta" style="float: right ; " class="button_cursor" onclick="if(document.getElementById('index_select').value>1){document.getElementById('index_select').value--;get_data(document.getElementById('index_select').value)}">Echipa precedenta</button>
+
+<div id="players_data" ></div>
+<?php require_once('footer.php') ?>
+  

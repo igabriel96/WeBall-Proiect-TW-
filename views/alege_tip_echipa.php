4fcac981ca2499986896bbcc98435052421a1da0
ioneseigabriel <ioneseigabriel@gmail.com>
2017-05-30 23:04:16 +0300

    Add files via upload

---
+++ views/alege_tip_echipa.php
@@ -0,0 +1,3 @@
+<?php require_once('header.php') ?>
+<a href="index.php?action=inregistreaza_echipa_reala" style="float:center;"><button>Register real team</button></a>
+<a href="index.php?action=inregistreaza_echipa_noua" style="float: center;"><button>Register new team</button></a>

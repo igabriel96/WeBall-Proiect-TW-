8d0ce007c38bca5d224cf03bba64bea70b4b558d
razvanvinatoru <razvan.vinatoru@icloud.com>
2017-05-30 13:18:11 +0300

    Update matches.php

--- views/matches.php
+++ views/matches.php
@@ -16,6 +16,8 @@ window.onload=get_data(1);
 	}
 </script>
 <?php require_once('header.php') ?>
+<a href="views/get_data_matches.php?tip=csv"><button id="d_csv" style="float: left" class="button_cursor" >CSV File</button></a>
+<a href="views/get_data_matches.php?tip=pdf"><button id="d_pdf" style="float: left" class="button_cursor" >PDF File</button></a>
 <button id="etapa_viitoare" class="button_cursor" onclick="if(document.getElementById('index_select').value<etape){document.getElementById('index_select').value++;get_data(document.getElementById('index_select').value)}" style="float: right;">Etapa viitoare</button><select id="index_select" onchange="get_data(document.getElementById('index_select').value)" style="float: right ;height: 30px ;width: 10%;"><?php for($i=1;$i<=$row[0];$i++) echo '<option value="'.$i.'">'.$i.'</option>';?></select><button id="etapa_trecuta" style="float: right ; " class="button_cursor" onclick="if(document.getElementById('index_select').value>1){document.getElementById('index_select').value--;get_data(document.getElementById('index_select').value)}">Etapa trecuta</button>
 <div id="matches_data" ></div>
 <?php require_once('footer.php') ?>

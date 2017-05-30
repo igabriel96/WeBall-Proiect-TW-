eee23a4b1cbbdb7afe090f26b539fdfafec6f1fa
razvanvinatoru <razvan.vinatoru@icloud.com>
2017-05-30 15:03:25 +0300

    Update ranking.php

--- views/ranking.php
+++ views/ranking.php
@@ -19,6 +19,8 @@ window.onload=get_data(1);
 	}
 </script>
 <?php require_once('header.php') ?>
+<a href="views/get_data_ranking.php?tip=csv"><button id="d_csv" style="float: left" class="button_cursor" >CSV File</button></a>
+<a href="views/get_data_ranking.php?tip=pdf"><button id="d_pdf" style="float: left" class="button_cursor" >PDF File</button></a>
 <button id="etapa_viitoare" class="button_cursor" onclick="if(document.getElementById('index_select').value<etape){document.getElementById('index_select').value++;get_data(document.getElementById('index_select').value)}" style="float: right;">Grupa urmatoare</button><select id="index_select" onchange="get_data(document.getElementById('index_select').value)" style="float: right ;height: 30px ;width: 10%;">
 <?php for($i=1;$i<=$row[0];$i++) 
     echo '<option value="'.$i.'">'.$i.'</option>';

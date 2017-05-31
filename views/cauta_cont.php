2da5407c0721a8d16dd5e60bf7093a3d3868b249
alinapetrei <synceed96@gmail.com>
2017-05-31 04:29:48 +0300

    Update cauta_cont.php

--- views/cauta_cont.php
+++ views/cauta_cont.php
@@ -1,57 +1,15 @@
-
+<!DOCTYPE html>
 <?php require_once('header.php') ?>
 
 
-<form class="modal-content animate" action="http://localhost:8989/TW/index.php?action=cauta_cont&filter_on=1" method="POST">
+<form class="modal-content animate" action="http://localhost:8181/TW/index.php?action=afisare_cont" method="POST">
  
-	
-    <div class="container">
-      <label><b>Account Name</b></label>
-      <input type="text"  maxlength="20" placeholder="Name"  name="nume_cont" required>
-      <button type="Submit">Search</button>
+	<div class="container">
+    <label><b>Account Username</b></label>
+    <input type="text"  maxlength="30" placeholder="userame"  name="username" >
+    <button type="Submit">Search</button>
     </div>
-
-
-
-<?php
-if(isset($_REQUEST['filter_on'])) {
-
-	//print_r ($_REQUEST['nume_cont']);
-	$v_nume_cont=$_REQUEST['nume_cont'];
-	$password=$_REQUEST['password'];
-	$sql = " select * FROM UTILIZATOR where username = '";
-	$sql.="$v_nume_cont";
-	$sql.="'";
-	$result = oci_parse($db, $sql);
-	oci_execute($result);
-	$r = oci_fetch_row($result);
-	if($r) {
-	print_r('ID ul este ');
-	print_r($r[0]);
-	?> <br> <?php
-	print_r('Username ul este ');
-	print_r($r[1]);
-	?> <br> <?php
-	print_r('Parola este ');
-	print_r($r[2]);
-	?> <br> <?php
-	print_r('Emailul este ');
-	print_r($r[3]);
-	?> <br> <?php
-	print_r('Rolul este de ');
-	print_r($r[4]);	
 	
-	?> <br> <?php
-	} else
-print_r('Contul nu a fost gasit');}
+</form>
 
-    
-
-
-
-
-
-
-    
-
-
+<?php require_once('footer.php') ?>

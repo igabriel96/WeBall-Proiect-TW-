9ed4a9d8cf5923b1d91599a481d9e937be9afbb5
ioneseigabriel <ioneseigabriel@gmail.com>
2017-06-01 21:53:08 +0300

    Update optiuni_organizatorii.php

--- views/optiuni_organizatorii.php
+++ views/optiuni_organizatorii.php
@@ -1,5 +1,14 @@
 <?php require_once('header.php') ?>
+<?php 
+$sql="Select organizare_campionat.getTipCampionat() from dual";
+$statement=oci_parse($db,$sql);
+oci_execute($statement);
+$row=oci_fetch_array($statement);
+if($row[0]!='necunoscut')
+{
+?>
 <a href="index.php?action=alege_tip_echipa" style="float:center;"><button>Register team</button></a>
 <a href="index.php?action=inregistreaza_jucator" style="float: center;"><button>Register player</button></a>
-<a href="index.php?action=seteaza_grupe" style="float: center;"><button>Set groups</button></a>
 <a href="index.php?action=sterge_echipa" style="float: center;"><button>Set teams</button></a>
+<?php } ?>
+<a href="index.php?action=seteaza_grupe" style="float: center;"><button>Set Organizational model</button></a>

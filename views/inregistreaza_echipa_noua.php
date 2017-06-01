d961a1354496727798e33094fb895e76e66eb7c5
alinapetrei <synceed96@gmail.com>
2017-06-02 02:48:46 +0300

    Update inregistreaza_echipa_noua.php

--- views/inregistreaza_echipa_noua.php
+++ views/inregistreaza_echipa_noua.php
@@ -1,12 +1,38 @@
 <?php require_once('header.php') ?>
-<form class="modal-content animate" action="index.php" method="POST">
+
+<?php
+
+if(!isset($_REQUEST['nameofteam']))
+{?>
+
+<form class="modal-content animate" action="index.php?action=inregistreaza_echipa_noua&filter_on=1" method="POST">
  
     <div class="container">
-      <input type="hidden" name="action" value="insert_echipa"></input>
       <label><b>Name team</b></label>
-      <input type="text"  maxlength="20"  name="name" required>
+      <input type="text" placeholder="Insert the name of the team" maxlength="20"  name="nameofteam" pattern="[a-zA-Z].{2,}" title="The name of the team should have at least three letters">
       <label><b>Country</b></label>
-      <input type="text"  maxlength="20" name="tara" required>
+      <input type="text"  placeholder="Insert the country of the team" maxlength="20" name="country" pattern="[a-zA-Z].{3,}" title="The name of the team should have at least four letters">
+      <label><b>Number of players</b></label>
+      <input type="number" name="numberofplayers" min="11" max="30"><br>
+      <label><b>Group number</b></label>
+      <input type="number" name="groupnumber" min="1" max="<?php echo $row[0] ?>"> 
       <button type="Submit">Register Team</button>
     </div>  
   </form>
+    
+<?php }  else { 
+    if(isset($_REQUEST['filter_on'])) {
+    
+	$teamname = $_REQUEST['nameofteam'];
+    $country = $_REQUEST['country'];
+    $numberofplayers = $_REQUEST['numberofplayers'];
+    $groupnumber = $_REQUEST['groupnumber'];
+    $sql="insert into echipe(nume,logo,tara,numar_jucatori,id_grupa) values('".$teamname."','http://www.s-cup.it/public/squadre/new-team.png','".$country."','".$numberofplayers."','".$groupnumber."')";
+    $statement=oci_parse($db,$sql);
+    $result=oci_execute($statement);
+     echo '<p style="color: white;
+        text-align: center;margin-top : 5%">Ai inregistrat echipa cu succes!</p>';    
+}
+}?>
+
+

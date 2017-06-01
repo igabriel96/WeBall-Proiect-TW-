e4481392c3b2820bb9726e37a3138ba199a72c5e
alinapetrei <synceed96@gmail.com>
2017-06-02 01:13:32 +0300

    Update update_account.php

--- views/update_account.php
+++ views/update_account.php
@@ -1,35 +1,56 @@
 <?php require_once('header.php') ?>
 
-
-<form class="modal-content animate" action="index.php?action=update_account&filter_on=1" method="POST">
- 
-	
-    <div class="container">
-      <label><b>Account Name</b></label>
-      <input type="text"  maxlength="20" placeholder="Account Name"  name="nume_cont" required>
-	  <label><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="password" required>
-	   <label><b> New Password</b></label>
-      <input type="password" placeholder="Enter Password" name=" new_password" required>
+<?php 
+    
+if(!isset($_REQUEST['password']) && !isset($_REQUEST['newpassword']))
+{?>
+<form class="modal-content animate" action="index.php?action=update_account&username=<?php echo $_REQUEST['username'];?>&filter_on=1" method="POST">
+ 	
+    <div class="container">   
+      <label><b>Old Password</b></label>
+      <input type="password"  placeholder="Enter Old Password"  name="password" required>
+	  <label><b>New Password</b></label>
+      <input type="password" placeholder="Enter New Password" name="newpassword" required>
+      <label><b>Confirm Password</b></label>    
+      <input type="password" placeholder="Confirm New Password" name="confirmpassword" required>
       <button type="Submit">Update account</button>
     </div>
-<?php
+<?php } else {
 if(isset($_REQUEST['filter_on'])) {
-
-	//print_r ($_REQUEST['nume_cont']);
-	$v_nume_cont=$_REQUEST['nume_cont'];
-	$password=$_REQUEST['password'];
-	$new_password=$_REQUEST['new_password'];
-	$sql = " UPDATE UTILIZATOR SET parola= '";
-	$sql.="$new_password";
-	$sql.="'";
-	$sql.=" where parola ='";
-	$sql.="$password";
-	$sql.="'";
-	$result = oci_parse($db, $sql);
-	oci_execute($result);
-	print_r('Contul a fost actualizat');
-	
-}
     
+	$password=$_REQUEST['password'];
+	$newpassword=$_REQUEST['newpassword'];
+	$sql="select parola from utilizator where username = '".$_REQUEST['username']."'";
+    $statement=oci_parse($db,$sql);
+    $result=oci_execute($statement);
+    $row = oci_fetch_row($statement);
+    if($_REQUEST['password'] != $row[0])
+    {
+         echo '<p style="color: white;
+         text-align: center;margin-top : 5%">Ai introdus o parola gresita!</p>';
+    }
+    else
+    if($_REQUEST['newpassword'] != $_REQUEST['confirmpassword'])
+    {
+        echo '<p style="color: white;
+        text-align: center;margin-top : 5%">Cele doua parole nu se potrivesc!</p>';
+    }
+    else
+     if($_REQUEST['password'] == $_REQUEST['newpassword'])
+    {
+        echo '<p style="color: white;
+        text-align: center;margin-top : 5%">Parola noua nu poate fi aceeasi cu cea veche!</p>';
+    }
+    else
+    {
+        $sql="update utilizator set parola = '".$_REQUEST['newpassword']."' where
+        username ='".$_REQUEST['username']."'";
+        $statement=oci_parse($db,$sql);
+        $result=oci_execute($statement);
+        echo '<p style="color: white;
+        text-align: center;margin-top : 5%">Ai schimbat parola cu succes!</p>';
+    }
+}
+}?>
+
 

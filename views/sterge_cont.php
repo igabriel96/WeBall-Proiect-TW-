281055ca2952db2443cec949b472f68263b49a37
alinapetrei <synceed96@gmail.com>
2017-06-02 01:14:56 +0300

    Update sterge_cont.php

--- views/sterge_cont.php
+++ views/sterge_cont.php
@@ -1,24 +1,41 @@
 
 <?php require_once('header.php') ?>
+<?php require_once('class/class.user.php')?>
 
-<?php 
-   
-$sql="select rol from utilizator where username = '".$_REQUEST['username']."'";
-$statement=oci_parse($db,$sql);
-$result=oci_execute($statement);
+<?php
 $row = oci_fetch_row($statement);
-if($row[0] == admin)
+
+if($_REQUEST['username'] == $row[0])
 {
-    echo '<p style="color: white;
-    text-align: center;margin-top : 5%">Nu poti sterge contul unui alt admin!</p>';
+     
+     $sql="delete from utilizator where username = '".$row[0]."'";
+     $statement=oci_parse($db,$sql);
+     $result=oci_execute($statement);
+     echo '<p style="color: white;
+     text-align: center;margin-top : 5%">Stergerea contului s-a realizat cu succes!</p>';
+     logout();
+     die();
 }
 else
 {
-    $sql="delete from utilizator where username = '".$_REQUEST['username']."'";
+
+    $sql="select rol from utilizator where username = '".$_REQUEST['username']."'";
     $statement=oci_parse($db,$sql);
     $result=oci_execute($statement);
-     echo '<p style="color: white;
-    text-align: center;margin-top : 5%">Stergerea userului s-a realizat cu succes!</p>';
+    $row = oci_fetch_row($statement);
+    if($row[0] == admin)
+        {
+            echo '<p style="color: white;
+            text-align: center;margin-top : 5%">Nu poti sterge contul unui  alt admin!</p>';
+        }
+    else
+        {
+        $sql="delete from utilizator where username = '".$_REQUEST['username']."'";
+        $statement=oci_parse($db,$sql);
+        $result=oci_execute($statement);
+        echo '<p style="color: white;
+        text-align: center;margin-top : 5%">Stergerea userului s-a realizat cu succes!</p>';
+}
 }
 ?>
 <?php require_once('footer.php') ?>

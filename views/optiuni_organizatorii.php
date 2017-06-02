dab6aba0f329f48bd0308a5df8def93d8862f594
ioneseigabriel <ioneseigabriel@gmail.com>
2017-06-03 00:15:03 +0300

    Update optiuni_organizatorii.php

--- views/optiuni_organizatorii.php
+++ views/optiuni_organizatorii.php
@@ -7,10 +7,24 @@ $row=oci_fetch_array($statement);
 if($row[0]!='necunoscut')
 {
 ?>
-<a href="index.php?action=alege_tip_echipa" style="float:center;"><button>Register team</button></a>
-<a href="index.php?action=inregistreaza_jucator" style="float: center;"><button>Register player</button></a>
-<a href="index.php?action=sterge_echipa" style="float: center;"><button>Set teams</button></a>
-<a href="index.php?action=sterge_baza_date" style="float: center;"><button>Delete database</button></a>
+	<?php 
+		$sql="Select * from global_date";
+		$statement=oci_parse($db,$sql);
+		oci_execute($statement);
+		$row2=oci_fetch_array($statement);
+		if($row2['TIP_CAMPIONAT']=='campionat'&&$row2['NR_MAXIM_ECHIPE_CAMPIONAT']>$row2['NR_ECHIPE_CAMPIONAT'])
+			echo '<a href="index.php?action=alege_tip_echipa" style="float:center;"><button>Register team</button></a>';
+		if($row2['TIP_CAMPIONAT']=='cupa'&&$row2['NR_MAXIM_ECHIPE_CUPA']>$row2['NR_ECHIPE_CUPA']*4)
+			echo '<a href="index.php?action=alege_tip_echipa" style="float:center;"><button>Register team</button></a>';
+		if($row2['TIP_CAMPIONAT']=='campionat'&&$row2['NR_MAXIM_ECHIPE_CAMPIONAT']=$row2['NR_ECHIPE_CAMPIONAT'])
+			echo '<a href="index.php?action=creaza_etape" style="float:center;"><button>Stabileste meciuri</button></a>';
+		if($row2['TIP_CAMPIONAT']=='cupa'&&$row2['NR_MAXIM_ECHIPE_CUPA']=$row2['NR_ECHIPE_CUPA']*4)
+			echo '<a href="index.php?action=creaza_etape" style="float:center;"><button>Stabileste_meciuri</button></a>';
+	?>
+
+	<a href="index.php?action=inregistreaza_jucator" style="float: center;"><button>Register player</button></a>
+	<a href="index.php?action=sterge_echipa" style="float: center;"><button>Set teams</button></a>
+	<a href="index.php?action=delete_database" style="float: center;"><button>Delete database</button></a>
 <?php }else{ ?>
 
 <a href="index.php?action=seteaza_model_organizational" style="float: center;"><button>Set Organizational model</button></a>

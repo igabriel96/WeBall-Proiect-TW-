90d1ff8766f1529c1e8e9f140c7b3e93aec224f7
alinapetrei <synceed96@gmail.com>
2017-06-02 03:50:45 +0300

    Update inregistreaza_echipa_noua.php

--- views/inregistreaza_echipa_noua.php
+++ views/inregistreaza_echipa_noua.php
@@ -12,10 +12,10 @@ if(!isset($_REQUEST['nameofteam']))
       <input type="text" placeholder="Insert the name of the team" maxlength="20"  name="nameofteam" pattern="[a-zA-Z].{2,}" title="The name of the team should have at least three letters">
       <label><b>Country</b></label>
       <input type="text"  placeholder="Insert the country of the team" maxlength="20" name="country" pattern="[a-zA-Z].{3,}" title="The name of the team should have at least four letters">
-      <label><b>Number of players</b></label>
-      <input type="number" name="numberofplayers" placeholder="Insert the number of players" min="11" max="30"><br>
-      <label><b>Group number</b></label>
-      <input type="number" name="groupnumber" placeholder="Insert the group number" min="1" max="<?php echo $row[0] ?>"> 
+      <label><b>Number of players</b></label><br>
+      <input type="number" name="numberofplayers" placeholder="Number of players" min="11" max="30"><br>
+      <label><b>Group number</b></label><br>
+      <input type="number" name="groupnumber" placeholder="Group number" min="1" max="<?php echo $row[0] ?>"> 
       <button type="Submit">Register Team</button>
     </div>  
   </form>
@@ -34,5 +34,15 @@ if(!isset($_REQUEST['nameofteam']))
         text-align: center;margin-top : 5%">Ai inregistrat echipa cu succes!</p>';    
 }
 }?>
-
-
+<style>
+    input[type=number]{
+    width: 17%;
+    padding: 10px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+        
+    }
+</style>
+<?php require_once('views/footer.php');

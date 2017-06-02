8d2ff6cb40551b1c7efac2793a0f2cd7a7aecddc
alinapetrei <synceed96@gmail.com>
2017-06-03 02:16:44 +0300

    Add files via upload

---
+++ views/edit_review.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php require_once('header.php') ?>
+
+<form class="modal-content animate" action="http://localhost:8181/TW-TEST/index.php?action=reviews&id_meci=1" method="POST" style="width: 50%;height:40%">
+ 
+	<div class="container">
+    <label><b><center>Edit your review</center></b></label>
+    <input type="hidden" name="id_review" value="<?php echo $_REQUEST['id_review'];?>">    
+    <input type="text"  maxlength="30" placeholder="Insert text..."  name="editreview">
+    <button type="Submit">Edit</button>
+    </div>
+	
+</form>
+
+<?php require_once('footer.php') ?>

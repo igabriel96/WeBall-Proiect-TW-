30d1dccd54d1f6308fa2df29daf2e818b8f649e2
ioneseigabriel <ioneseigabriel@gmail.com>
2017-06-03 20:38:45 +0300

    Update poll.php

--- views/poll.php
+++ views/poll.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
 <?php require_once('header.php') ?>
-<html>
-<body>
-<div class="poll-body">    
-<fieldset>
-	<legend>Which team is better?</legend>
-	<form action="index.php?action=pollresults" id="form1" name="form1" method="POST">
-		<label>
-			<input type="radio" name="Poll" value="barcelona" id="Poll_0" />
-			Barcelona
-		 </label>
-		<label>
-			<input type="radio" name="Poll" value="realmadrid" id="Poll_1" />
-			Real Madrid
-		</label>
-            <label>
-			<input type="radio" name="Poll" value="equal" id="Poll_2" />
-			Are equal
-		</label>
-		<input type="submit" name="submit" id="submit" value="Submit vote!" />
+<fieldset style="width: 50%;text-align: center" >
+	<?php
+		$sql="Select * from poll where id_meci=".$_REQUEST['id_meci'];
+		$statement=oci_parse($db, $sql);
+		oci_execute($statement);
+		$row=oci_fetch_array($statement);
+		 ?>
+	<legend><?php echo $row['INTREBARE']; ?></legend>
+	<form action="index.php?show_poll" id="form1" name="form1" method="GET" style="text-align: left">
+		<?php
+		$contor=1;
+		while(isset(($row['RASPUNS'.$contor])))
+		{
+			echo '<label>';
+			echo '<input type="radio" name="Poll" value="'.$row['RASPUNS'.$contor].'" id="'.$contor.'" />';
+			echo $row['RASPUNS'.$contor];
+		 echo '</label>';
+		 echo '<br>';
+		 $contor++;
+		}
+		?>
+		<div id="button_submit" style="text-align: center">
+		<input type="submit" name="submit" id="submit" value="Vote" style=" width: 15% ;height: 10%" />
+		</div>
 		<input type="hidden" name="id" value="form1" />
 		<input type="hidden" name="MM_insert" value="form1" />
 	</form>
 </fieldset>
-</div>
-</body>
-</html>

b509c8d6e84a68a1be2cef09d8020fa9e11224b1
alinapetrei <synceed96@gmail.com>
2017-06-03 23:24:26 +0300

    Update search_account.php

--- views/search_account.php
+++ views/search_account.php
@@ -2,7 +2,7 @@
 <?php require_once('header.php') ?>
 
 
-<form class="modal-content animate" action="http://localhost:8181/TW/index.php?action=afisare_cont" method="POST" style="width:25%">
+<form class="modal-content animate" action="index.php?action=afisare_cont" method="POST" style="width:25%">
  
 	<div class="container">
     <label style="margin-left:23%"><b>Account Username</b></label>

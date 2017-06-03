e785f651bfeed853a8cfd1556ccc730c28692690
alinapetrei <synceed96@gmail.com>
2017-06-03 03:31:08 +0300

    Update reviews.php

--- views/reviews.php
+++ views/reviews.php
@@ -75,7 +75,9 @@ $sql="Select review.id , username ,data_review,text from review join utilizator 
                 <p> <?php echo oci_result($statement,'TEXT'); ?></p>
               </div>
               <div class="review-edit-delete">
-                  <a href="http://localhost:8181/TW/index.php?action=edit_review&id_review=<?php echo oci_result($statement,'ID')?>"style="text-decoration:none;color: white">Edit</a>
+                   <a href="http://localhost:8181/TW/index.php?action=edit_review&id_review=
+                   <?php echo oci_result($statement,'ID')?>&id_meci=
+                   <?php echo $_REQUEST['id_meci']?>"style="text-decoration:none;color: white">Edit</a>
               </div> 
               <div class="review-edit-delete">
                  Delete

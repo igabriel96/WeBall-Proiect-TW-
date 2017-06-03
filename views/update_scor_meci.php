e6daa07f1228e7cfe1c463ba97363812afb8f3d7
alinapetrei <synceed96@gmail.com>
2017-06-03 23:17:04 +0300

    Update update_scor_meci.php

--- views/update_scor_meci.php
+++ views/update_scor_meci.php
@@ -1,11 +1,12 @@
 <?php require_once('header.php') ?>
-<form class="modal-content animate" action="index.php?action=update_scor_meci&id_meci=<?php echo $_REQUEST['id_meci']?>&set_score=1" method="POST">
+<form class="modal-content animate" action="index.php?action=update_scor_meci&id_meci=
+      <?php echo $_REQUEST['id_meci']?>&set_score=1" method="POST" style="width:25%">
  
     <div class="container">
-      <label><b>Points host team</b></label>
-      <input type="text" pattern="[0-9]+" maxlength="3"  name="goluri_echipa_gazda" required>
-      <label><b>Points guest team</b></label>
-      <input type="text" pattern="[0-9]+" maxlength="3" name="goluri_echipa_oaspete" required>
+      <label style="margin-left: 26%"><b>Points host team</b></label>
+      <input type="text" pattern="[0-9]+" maxlength="2"  name="goluri_echipa_gazda" required>
+      <label style="margin-left: 26%"><b>Points guest team</b></label>
+      <input type="text" pattern="[0-9]+" maxlength="2" name="goluri_echipa_oaspete" required>
       <button type="SET SCORE">Set Score</button>
     </div>
     

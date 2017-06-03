302d33b8e24e81b3c977fa7533b9be1beb773035
alinapetrei <synceed96@gmail.com>
2017-06-03 23:23:28 +0300

    Update search_account.php

--- views/search_account.php
+++ views/search_account.php
@@ -2,10 +2,10 @@
 <?php require_once('header.php') ?>
 
 
-<form class="modal-content animate" action="http://localhost:8181/TW/index.php?action=afisare_cont" method="POST">
+<form class="modal-content animate" action="http://localhost:8181/TW/index.php?action=afisare_cont" method="POST" style="width:25%">
  
 	<div class="container">
-    <label><b>Account Username</b></label>
+    <label style="margin-left:23%"><b>Account Username</b></label>
     <input type="text"  maxlength="30" placeholder="Insert username"  name="username" >
     <button type="Submit">Search</button>
     </div>

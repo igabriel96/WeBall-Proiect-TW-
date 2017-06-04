88ee7a56e184f458cdabb04a55742f84e1343feb
ioneseigabriel <ioneseigabriel@gmail.com>
2017-06-04 13:33:20 +0300

    Update poll.php

--- views/poll.php
+++ views/poll.php
@@ -1,30 +1,50 @@
 <!DOCTYPE html>
 <?php require_once('header.php') ?>
-<fieldset style="width: 25%;text-align: center" >
-	<?php
-		$sql="Select * from poll where id_meci=".$_REQUEST['id_meci'];
+<?php 
+	$sql="Select count(*),id from poll where id_meci=".$_REQUEST['id_meci'].' group by id';
+	$statement=oci_parse($db, $sql);
+	oci_execute($statement);
+	$row=oci_fetch_array($statement);
+	if($row[0]==0)
+	{
+		echo '<div style="text-align:center">Momentan nu exista un poll pentru acest meci</div>';
+	}
+	else
+	{
+		$sql="Select count(*) from vote_poll where id_poll=".$row[1].' and id_utilizator='.$_SESSION['uid'];
 		$statement=oci_parse($db, $sql);
 		oci_execute($statement);
 		$row=oci_fetch_array($statement);
-		 ?>
-	<legend><?php echo $row['INTREBARE']; ?></legend>
-	<form action="index.php?show_poll" id="form1" name="form1" method="GET" style="text-align: left;color:white">
-		<?php
-		$contor=1;
-		while(isset(($row['RASPUNS'.$contor])))
+		if($row[0]==0)
 		{
-			echo '<label>';
-			echo '<input type="radio" name="Poll" value="'.$row['RASPUNS'.$contor].'" id="'.$contor.'" />';
-			echo $row['RASPUNS'.$contor];
-		 echo '</label>';
-		 echo '<br>';
-		 $contor++;
-		}
-		?>
-		<div id="button_submit" style="text-align: center">
-		<input type="submit" name="submit" id="submit" value="Vote" style=" width: 15% ;height: 10%" />
-		</div>
-		<input type="hidden" name="id" value="form1" />
-		<input type="hidden" name="MM_insert" value="form1" />
-	</form>
-</fieldset>
+	?>
+			<fieldset style="width: 25%;text-align: center" >
+				<?php
+					$sql="Select * from poll where id_meci=".$_REQUEST['id_meci'];
+					$statement=oci_parse($db, $sql);
+					oci_execute($statement);
+					$row=oci_fetch_array($statement);
+					 ?>
+				<legend><?php echo $row['INTREBARE']; ?></legend>
+				<form action="index.php?action=insert_vote_poll" id="form1" name="form1" method="POST" style="text-align: left;color:white">
+				<input type="hidden" name="id_poll" value="<?php echo $row['ID']; ?>">
+					<?php
+					$contor=1;
+					while(isset(($row['RASPUNS'.$contor])))
+					{
+						echo '<label>';
+						echo '<input type="radio" name="Poll" value="'.$contor.'" id="'.$contor.'" />';
+						echo $row['RASPUNS'.$contor];
+					 echo '</label>';
+					 echo '<br>';
+					 $contor++;
+					}
+					?>
+					<div id="button_submit" style="text-align: center">
+					<input type="submit" name="submit" id="submit" value="Vote" style=" width: 15% ;height: 10%" />
+					</div>
+					<input type="hidden" name="id" value="form1" />
+					<input type="hidden" name="MM_insert" value="form1" />
+				</form>
+			</fieldset>
+	<?php }}?>

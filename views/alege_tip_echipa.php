bcc76edc74a7ef22afda195e4fcbacbab0b7eb04
alinapetrei <synceed96@gmail.com>
2017-06-04 04:08:20 +0300

    Update alege_tip_echipa.php

--- views/alege_tip_echipa.php
+++ views/alege_tip_echipa.php
@@ -1,3 +1,14 @@
 <?php require_once('header.php') ?>
-<a href="index.php?action=inregistreaza_echipa_reala" style="float:center;"><button>Register real team</button></a>
-<a href="index.php?action=inregistreaza_echipa_noua" style="float: center;"><button>Register new team</button></a>
+<div class="organisation-options">
+<p style="color:white;text-align:center;margin-top:2%">Options</p>
+<a href="index.php?action=inregistreaza_echipa_reala" style="color:white;text-align:center;margin-top:2%"><button>Register real team</button></a>
+<a href="index.php?action=inregistreaza_echipa_noua" style="color:white;text-align:center;margin-top:2%"><button>Register new team</button></a>
+</div>
+<style>
+    .organisation-options{
+    margin: 5% 37% auto 37%;
+    width: 26%;
+    }
+</style>
+
+<?php require_once('footer.php') ?>

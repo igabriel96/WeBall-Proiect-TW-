6f25af563f6a167b2bd57315b6640b9323ab9d07
razvanvinatoru <razvan.vinatoru@icloud.com>
2017-06-04 17:33:52 +0300

    Update poll.php

--- views/poll.php
+++ views/poll.php
@@ -47,4 +47,58 @@
 					<input type="hidden" name="MM_insert" value="form1" />
 				</form>
 			</fieldset>
-	<?php }}?>
+	<?php }
+		
+		
+		else
+			{
+				$sql="select * from poll where id_meci=".$_REQUEST['id_meci'];
+				$statement1=oci_parse($db, $sql);
+				oci_execute($statement1);
+				$row1=oci_fetch_array($statement1);
+
+				$nr_raspunsuri[0]=0;$nr_raspunsuri[1]=0;$nr_raspunsuri[2]=0;$nr_raspunsuri[3]=0;$nr_raspunsuri[4]=0;$nr_raspunsuri[5]=0;
+				$nr_raspunsuri_totale=0;
+				$sql="select count(*) as contor,optiune from vote_poll where id_poll=".$row1['ID'] .' group by optiune';
+				$statement5=oci_parse($db,$sql);
+				oci_execute($statement5);
+				oci_execute($statement5);
+				while($row5=oci_fetch_array($statement5))
+				{
+                    $nr_raspunsuri[$row5['OPTIUNE']]=$row5['CONTOR'];
+                    $nr_raspunsuri_totale=$nr_raspunsuri_totale+ $row5['CONTOR'];
+				}
+				$sql="select intrebare,raspuns1,raspuns2,raspuns3,raspuns4 from poll where id_meci=".$_REQUEST['id_meci'];
+				$statement3=oci_parse($db, $sql);
+				oci_execute($statement3);
+				$row3=oci_fetch_array($statement3);
+				$contor=1;
+				echo '<fildset>';
+				echo '<legend> Results for "'.$row3[0].'"</legend>';
+				echo '<ul>';
+				while(isset($row3[$contor]))
+				{
+						echo '<li >';
+							echo '<span >'.$nr_raspunsuri[$contor] .' votes : <span>  '.$row3[$contor];
+							echo '<br/>';
+							if($nr_raspunsuri_totale>0)
+							{
+								echo '<div class="results-bar" style="width:'.round(round(($nr_raspunsuri[$contor]/($nr_raspunsuri_totale))*100,2)*(4/5),2).'%;">';
+								 	echo round(($nr_raspunsuri[$contor]/($nr_raspunsuri_totale))*100,2).'%';
+							}
+							else
+							{
+								echo '<div class="results-bar" style="width:0%;">';
+								 	echo '0%';
+							}
+								echo '</div>';
+
+						echo '</li>';
+						$contor++;
+				}
+				echo '</ul>';
+				echo '     <h6>Total votes:'.$nr_raspunsuri_totale.'</h6>';
+				echo "</fildset>";
+
+			}
+	}?>
